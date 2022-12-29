62dd4c78555f9fa336035ff5bf066f4f0d3291fa
Jose <josealejandrogilmendez08@gmail.com>
2022-12-29 19:41:20 -0400

    add tree class and create nodes.js file to node class

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-// class Tree {
-//   constructor(root) {
-//     this.root = root;
-//   }
-// }
+/* eslint import/extensions: [0, {  <js>: "always"  }] */
+import Node from './nodes.js';
 
 const mergeSort = (arr) => {
   if (arr.length < 2) return arr;
-
   const middleIndex = Math.ceil(arr.length / 2);
   const firstHalf = arr.splice(0, middleIndex);
   const secondHalf = arr.splice(-middleIndex);
-
   const a = mergeSort(firstHalf);
   const b = mergeSort(secondHalf);
   const c = [];
   const length = a.length + b.length;
-
   while (length !== c.length) {
-    if (a[0] > b[0] || a[0] === undefined) {
-      c.push(b.shift());
-    } else {
-      c.push(a.shift());
-    }
+    if (a[0] > b[0] || a[0] === undefined) c.push(b.shift());
+    else c.push(a.shift());
   }
   return c;
 };
@@ -49,6 +32,7 @@ const buildTree = (arr) => {
 
   return node;
 };
+
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
@@ -58,5 +42,12 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
-const root = buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-prettyPrint(root);
+
+class Tree {
+  constructor(array) {
+    this.root = buildTree(array);
+  }
+}
+
+const binaryTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(binaryTree.root);
