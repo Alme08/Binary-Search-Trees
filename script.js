f59b972629c40516fe7a675f9d0782c09a83c150
Jose <josealejandrogilmendez08@gmail.com>
2022-12-29 23:04:49 -0400

    add (minValue, insert, delete, find) methods

--- script.js
+++ script.js
@@ -33,21 +33,73 @@ const buildTree = (arr) => {
   return node;
 };
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-};
-
 class Tree {
   constructor(array) {
     this.root = buildTree(array);
   }
+
+  prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    if (node.right !== null) {
+      this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
+  minValue(root = this.root) {
+    let r = root;
+    let minV = r.data;
+    while (r.left != null) {
+      r = r.left;
+      minV = r.data;
+    }
+    return minV;
+  }
+
+  insert(data, root = this.root) {
+    let r = root;
+    if (r.data === data) {
+      console.log("Couldn't insert");
+      return r;
+    }
+    if (r === null) {
+      r = new Node(data);
+      return r;
+    }
+
+    if (data < r.data) r.left = this.insert(data, r.left);
+    else if (data > r.data) r.right = this.insert(data, r.right);
+    return r;
+  }
+
+  delete(data, root = this.root) {
+    const r = root;
+    if (r === null) return r;
+
+    if (data < r.data) r.left = this.delete(data, r.left);
+    else if (data > r.data) r.right = this.delete(data, r.right);
+    else {
+      if (r.left === null) return r.right;
+      if (r.right === null) return r.left;
+      r.data = this.minValue(r.right);
+      r.right = this.delete(r.data, r.right);
+    }
+    return r;
+  }
+
+  find(data, root = this.root) {
+    if (root === null) return root;
+
+    if (data < root.data) return this.find(data, root.left);
+    if (data > root.data) return this.find(data, root.right);
+
+    return root;
+  }
 }
 
 const binaryTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-prettyPrint(binaryTree.root);
+// binaryTree.delete(4);
+binaryTree.prettyPrint();
+console.log(binaryTree.find(7));
